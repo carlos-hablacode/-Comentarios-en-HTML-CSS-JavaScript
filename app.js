a10e8ee1304c068edf53d4d1a76a47bffdebe8c7
carlos-hablacode <58349988+carlos-hablacode@users.noreply.github.com>
2019-11-29 21:14:00 -0600

    Agregar la configuración final del proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+const funcion_de_boton = () => {
+    document.getElementById('ejemplo').innerHTML = "Hola Mundo"
+}
+
+
+/* ---Comentarios multilínea----
+const funcionCoches = () => {
+    alert(carros)
+}
+*/
+
+// Comentarios en línea
+// const carros = ["Toyota", "BMW", "Ford"];
